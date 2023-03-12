886d974421842a2dcc37785ec3e78ab15d6fe37d
netanel-haber-deci <netanel.haber@deci.ai>
2023-03-12 21:22:29 +0200

    tests pass

--- a1/decoders/regToReg.ts
+++ a1/decoders/regToReg.ts
@@ -1,5 +1,5 @@
 import { assert, assertNotEquals } from "assert";
-import { getBits } from "../bitManipulation.ts";
+import { getBits, twoByteNumber } from "../bitManipulation.ts";
 import { computeSourceDestination, Decoder, getRegisters } from "./common.ts";
 
 const getRm = (b2: number) => getBits(b2, { msb: 2, lsb: 0 });
@@ -19,30 +19,61 @@ function registerMode(b1: number, b2: number) {
   return [`mov ${destination}, ${source}`, 2] as const;
 }
 
-const RM: Array<[string] | [string, string] | []> = [
+const rmToRegister: Array<[string] | [string, string]> = [
   ["bx", "si"],
   ["bx", "di"],
   ["bp", "si"],
   ["bp", "di"],
   ["si"],
   ["di"],
-  [],
+  ["bp"],
   ["bx"],
 ];
 
-function memoryNoDisplacement(b1: number, b2: number) {
+function memory(
+  b1: number,
+  b2: number,
+  ...bytes: number[]
+) {
+  assert(bytes.length <= 2);
   const registers = getRegisters(b1);
   const { reg, rm } = getRegAndRm(b2);
-  assertNotEquals(rm, 6);
 
-  const rmStr = RM[rm];
+  const rmStr = rmToRegister[rm];
+
+  const calculation = rmStr.concat(
+    bytes.length === 0
+      ? []
+      : [bytes.length === 1 ? bytes[0] : twoByteNumber(bytes[0], bytes[1])]
+        .filter(Boolean).map(
+          String,
+        ),
+  );
+
   const { destination, source } = computeSourceDestination(
     b1,
     registers[reg],
-    `[${rmStr.join(" + ")}]`,
+    `[${calculation.join(" + ")}]`,
   );
-  return [`mov ${destination}, ${source}`, 2] as const;
+  return [`mov ${destination}, ${source}`, arguments.length] as const;
 }
+
+const RM_SPECIAL_CASE_NO_DISPLACEMENT = 0b110;
+
+const modeNoDisplacement = (b1: number, b2: number) => {
+  assertNotEquals(getRm(b2), RM_SPECIAL_CASE_NO_DISPLACEMENT);
+  return memory(b1, b2);
+};
+
+const mode8BitDisplacement = (b1: number, b2: number, b3: number) =>
+  memory(b1, b2, b3);
+
+const mode16BitDisplacement = (
+  b1: number,
+  b2: number,
+  b3: number,
+  b4: number,
+) => memory(b1, b2, b3, b4);
 
 enum Mode {
   MEMORY_NO_DISPLACEMENT = 0b00,
@@ -61,11 +92,11 @@ export const regMemory: Decoder = (asm, p) => {
     case Mode.REGISTER:
       return registerMode(b1, b2);
     case Mode.MEMORY_NO_DISPLACEMENT:
-      return memoryNoDisplacement(b1, b2);
+      return modeNoDisplacement(b1, b2);
     case Mode.MEMORY_8_DISPLACEMENT:
-      throw new Error("8 disp not implemented");
+      return mode8BitDisplacement(b1, b2, asm[p + 2]);
     case Mode.MEMORY_16_DISPLACEMENT:
-      throw new Error("16 disp not implemented");
+      return mode16BitDisplacement(b1, b2, asm[p + 2], asm[p + 3]);
   }
   throw new Error(`${mode} not found`);
 };
