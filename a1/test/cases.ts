25ac88b0b576c12995ffab5393f37ee896d4af63
netanel-haber <netanel.t.haber@gmail.com>
2023-03-25 02:26:08 +0300

    done with 41

--- a1/test/cases.ts
+++ a1/test/cases.ts
@@ -3,6 +3,8 @@ const A38 = "computer_enhance/perfaware/part1/listing_0038_many_register_mov";
 const A39 = "computer_enhance/perfaware/part1/listing_0039_more_movs";
 const A40 = "computer_enhance/perfaware/part1/listing_0040_challenge_movs";
 const A41 = "computer_enhance/perfaware/part1/listing_0041_add_sub_cmp_jnz";
+// cat all of the files above.
+// const ALL = "computer_enhance/perfaware/part1/all";
 
 export const cases = {
   A37,
@@ -10,4 +12,5 @@ export const cases = {
   A39,
   A40,
   A41,
+  // ALL,
 } as const;
